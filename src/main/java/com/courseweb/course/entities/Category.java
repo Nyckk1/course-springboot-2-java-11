521d557842c8c83c7fa1936c97003494ccf18e81
nycolas mendes <nycolascouto1@gmail.com>
2022-01-23 01:25:24 -0300

    OrderItem, many-to-many association with extra attributes - PART 1

--- src/main/java/com/courseweb/course/entities/Category.java
+++ src/main/java/com/courseweb/course/entities/Category.java
@@ -15,20 +15,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
+public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -52,11 +52,10 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -82,6 +81,5 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-
 
 }
