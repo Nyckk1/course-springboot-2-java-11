8210aefcfde19675e08a73e8f8a3e91e168b2d66
nycolas mendes <nycolascouto1@gmail.com>
2022-01-26 00:32:27 -0300

    Exception handling - delete

--- src/main/java/com/courseweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/courseweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.courseweb.course.services.exceptions.DatabaseException;
 import com.courseweb.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,4 +23,15 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 		
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
 }
+
